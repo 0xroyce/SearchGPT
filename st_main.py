f5e1f9ff6307c996b220855251625eae6fc8fa96
seedgularity <131738679+seedgularity@users.noreply.github.com>
2023-06-17 18:17:27 +0200

    Update st_main.py

--- st_main.py
+++ st_main.py
@@ -13,9 +13,11 @@ else:
     secrets = st.secrets
 
 # Set API keys and model
-os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
-os.environ["BROWSERLESS_API_KEY"] = secrets["BROWSERLESS_API_KEY"]
-os.environ["SERPAPI_API_KEY"] = secrets["SERPAPI_API_KEY"]
+st.write(
+    os.environ["OPENAI_API_KEY"] == secrets["OPENAI_API_KEY"],
+    os.environ["BROWSERLESS_API_KEY"] == secrets["BROWSERLESS_API_KEY"],
+    os.environ["SERPAPI_API_KEY"] == secrets["SERPAPI_API_KEY"]
+)
 
 open_ai_api_key = secrets["OPENAI_API_KEY"]
 browserless_api_key = secrets["BROWSERLESS_API_KEY"]
