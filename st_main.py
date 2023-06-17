2490d81a3ac0ceba5f8551b5b1c56f499e8c1105
seedgularity <131738679+seedgularity@users.noreply.github.com>
2023-06-17 19:43:22 +0200

    Update st_main.py

--- st_main.py
+++ st_main.py
@@ -118,11 +118,14 @@ def main():
             for i in range(4):
                 time.sleep(0.1)
                 if i < len(links):
+                    st.write(f"Step {2*i+1}: Scraping link {i+1}")
                     webpages.append(scrape(links[i]))
                 time.sleep(0.1)
                 if i < len(webpages):
+                    st.write(f"Step {2*i+2}: Summarizing webpage {i+1}")
                     summaries.append(summarize(question, webpages[i]))
 
+            st.write("Step 9: Generating final summary")
             answer = final_summary(question, summaries)
 
             st.write("HERE IS THE ANSWER")
